08d4b099ebf32b9c758ee75d78c6d5b0133e917c
Buelent <buelent@rsnweb.ch>
2019-10-26 17:30:11 +0200

    Grundlage für User Objekt

--- SmartToDo/SmartToDo.Tests/ModelTests.cs
+++ SmartToDo/SmartToDo.Tests/ModelTests.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using SmartToDo.Models;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using NUnit.Framework;
+//using SmartToDo.Models;
 
 
-namespace SmartToDo.Tests
-{
-    [TestFixture]
-    public class ModelTests
-    {
-        [Test]
-        public void Test() {
-            var sut = new Test();
+//namespace SmartToDo.Tests
+//{
+//    //[TestFixture]
+//    //public class ModelTests
+//    //{
+//    //    [Test]
+//    //    public void Test() {
+//    //        var sut = new Test();
             
-            var result = sut.AddNumbers(5, 3);
+//    //        var result = sut.AddNumbers(5, 3);
 
-            const int expectedResult = 8;
-            Assert.That(result == expectedResult);
-        }
-    }
-}
+//    //        const int expectedResult = 8;
+//    //        Assert.That(result == expectedResult);
+//    //    }
+//    //}
+//}
---
+++ SmartToDo/SmartToDo/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartToDo.Models {
+    public class User {
+        //public int AddNumbers(int zahl1, int zahl2) {
+        //    var result = zahl1 + zahl2;
+        //    return result;
+        //}
+        // BenutzerName, Passwort, Rolle, E-Mail, Kürzel....
+    }
+}
