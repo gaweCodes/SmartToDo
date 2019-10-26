f025ca63691ab3cd84272be27ca27c2307fecb89
Buelent <buelent@rsnweb.ch>
2019-10-26 16:48:44 +0200

    Init

---
+++ SmartToDo/SmartToDo/Models/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartToDo.Models {
+    public class Test {
+        public int AddNumbers(int zahl1, int zahl2) {
+            var result = zahl1 + zahl2;
+            return result;
+        }
+    }
+}
